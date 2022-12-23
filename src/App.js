a2ee2d2e28644f4cb1f36b19eefe52be199068a8
Arash <aras.goodarzi@gmail.com>
2022-12-23 17:18:01 -0500

    add sidebar to page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+import Topbar from "./scence/global/Topbar";
+import Sidebar from "./scence/global/Sidebar";
+import Dashboard from "./scence/dashboard";
+import { MyProSidebarProvider } from "./scence/global/sidebarContext";
+
+// https://codesandbox.io/s/react-dashboard-pnm6fh?file=/src/theme.js:0-5199
+
+// import Sidebar from "./scence/global/Sidebar";
+// import Team from "./scence/team";
+// import Invoices from "./scence/invoices";
+// import Contacts from "./scence/contacts";
+// import Bar from "./scence/bar";
+// import Form from "./scence/form";
+// import Line from "./scence/line";
+// import Pie from "./scence/pie";
+// import FAQ from "./scence/faq";
+// import Geography from "./scence/geography";
+// import Calender from "./scence/calender";
 
 function App() {
+  const [theme, colorMode] = useMode();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <MyProSidebarProvider>
+          <div className="app">
+            {/* <Sidebar /> */}
+            <main className="content">
+              <Topbar />
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                {/* <Route path="/team" element={<Team />} /> */}
+                {/* <Route path="/invoices" element={<Invoices />} /> */}
+                {/* <Route path="/contacts" element={<Contacts />} /> */}
+                {/* <Route path="/bar" element={<Bar />} /> */}
+                {/* <Route path="/form" element={<Form />} /> */}
+                {/* <Route path="/line" element={<Line />} /> */}
+                {/* <Route path="/pie" element={<Pie />} /> */}
+                {/* <Route path="/faq" element={<FAQ />} /> */}
+                {/* <Route path="/geography" element={<Geography />} /> */}
+                {/* <Route path="/calender" element={<Calender />} /> */}
+              </Routes>
+            </main>
+          </div>
+        </MyProSidebarProvider>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 

7afd0de56c24afe1dbf05027bf21f2b2aa6755d3
Arash <aras.goodarzi@gmail.com>
2023-05-12 12:03:16 -0400

    modifiation router for deploying

--- src/App.js
+++ src/App.js
@@ -16,36 +16,39 @@ import Bar from "./pages/bar";
 import Pie from "./pages/pie";
 import Line from "./pages/line";
 import Geography from "./pages/geography";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   const [theme, colorMode] = useMode();
   return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <MyProSidebarProvider>
-          <div className="app">
-            {/* <Sidebar /> */}
-            <main className="content">
-              <Topbar />
-              <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/team" element={<Team />} />
-                <Route path="/contacts" element={<Contacts />} />
-                <Route path="/invoices" element={<Invoices />} />
-                <Route path="/form" element={<Form />} />
-                <Route path="/calendar" element={<Calendar />} />
-                <Route path="/faq" element={<FAQ />} />
-                <Route path="/bar" element={<Bar />} />
-                <Route path="/pie" element={<Pie />} />
-                <Route path="/line" element={<Line />} />
-                <Route path="/geography" element={<Geography />} />
-              </Routes>
-            </main>
-          </div>
-        </MyProSidebarProvider>
-      </ThemeProvider>
-    </ColorModeContext.Provider>
+    <BrowserRouter>
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <MyProSidebarProvider>
+            <div className="app">
+              {/* <Sidebar /> */}
+              <main className="content">
+                <Topbar />
+                <Routes>
+                  <Route path="/" element={<Dashboard />} />
+                  <Route path="/team" element={<Team />} />
+                  <Route path="/contacts" element={<Contacts />} />
+                  <Route path="/invoices" element={<Invoices />} />
+                  <Route path="/form" element={<Form />} />
+                  <Route path="/calendar" element={<Calendar />} />
+                  <Route path="/faq" element={<FAQ />} />
+                  <Route path="/bar" element={<Bar />} />
+                  <Route path="/pie" element={<Pie />} />
+                  <Route path="/line" element={<Line />} />
+                  <Route path="/geography" element={<Geography />} />
+                </Routes>
+              </main>
+            </div>
+          </MyProSidebarProvider>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
+    </BrowserRouter>
   );
 }
 

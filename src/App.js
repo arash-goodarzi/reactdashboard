426ee1dcfbeb920370276e3509bc4e1587371ae6
Arash <aras.goodarzi@gmail.com>
2023-05-13 13:57:15 -0400

    modifiation router for deploying

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ import { BrowserRouter } from "react-router-dom";
 function App() {
   const [theme, colorMode] = useMode();
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <ColorModeContext.Provider value={colorMode}>
         <ThemeProvider theme={theme}>
           <CssBaseline />
